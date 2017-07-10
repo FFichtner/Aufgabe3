bbab5946b1e289f41bb57daa0bb79c4aa4a0eecf
Dome2k14 K8bal <Dome2k14 K8bal>
2017-07-10 15:20:37 +0200

    test

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$iA1TrT28$axqqCz7zLqEisjTcXfjsH.:Dominik Köberl:dominik.koeberl10@yahoo.de:admin,user
