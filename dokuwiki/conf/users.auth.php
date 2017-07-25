ecf755815fa03d0e9cb01b11a3d99dc2c90bcff5
korbay <kor3i@hotmail.de>
2017-07-25 10:45:59 +0200

    Blub

--- dokuwiki/conf/users.auth.php
+++ dokuwiki/conf/users.auth.php
@@ -10,3 +10,5 @@
 
 
 admin:$1$iA1TrT28$axqqCz7zLqEisjTcXfjsH.:Dominik Köberl:dominik.koeberl10@yahoo.de:admin,user
+fsinfo:$1$EuUtQnU7$x/Udz91R76O8yT1PBifKI/:Fachschaft:lolichbinda@yahoo.de:fachschaft
+ieee:$1$Kx1XVyv4$3Kus3LJYxn32sxF.PPFoz.:ieee:j178404@mvrht.net:ieee
